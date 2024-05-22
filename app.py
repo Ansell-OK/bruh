1a061dd4a464a84225ab408c174e7eb6ba559ce4
unknown <ansellokafor@gmail.com>
2024-05-22 17:04:54 +0100

    test

--- app.py
+++ app.py
@@ -7,17 +7,21 @@ model = tf.saved_model.load("dokinta_model")
 
 @app.route('/predict', methods=['POST'])
 def classify_text():
-    data = request.json 
-    text = data['text']
-    
-    text_tensor = tf.constant([text], shape=(1, 1), dtype=tf.string, name='input_2')
-    predictions = model(text_tensor, training=False)
-    text_argmax = tf.argmax(predictions, axis =1)
-    class_names = ['Common Cold', 'Dengue', 'Malaria', 'Typhoid']
-    prediction = class_names[text_argmax[0]]
+    try:
+        data = request.json 
+        text = data['text']
+        
+        text_tensor = tf.constant([text], shape=(1, 1), dtype=tf.string, name='input_2')
+        predictions = model(text_tensor, training=False)
+        text_argmax = tf.argmax(predictions, axis =1)
+        class_names = ['Common Cold', 'Dengue', 'Malaria', 'Typhoid']
+        prediction = class_names[text_argmax[0]]
 
+        
+        return jsonify(prediction)
     
-    return jsonify(prediction)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 @app.route('/')
