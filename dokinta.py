df7dd79f803378b3371d9e2cd1024b09e6cf1e68
unknown <ansellokafor@gmail.com>
2024-05-22 01:46:58 +0100

    first

---
+++ dokinta.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify, render_template
+import tensorflow as tf
+
+app = Flask(__name__)
+
+model = tf.saved_model.load("dokinta_model")
+
+@app.route('/predict', methods=['POST'])
+def classify_text():
+
+    data = request.json 
+    text = data['text']
+    
+    text_tensor = tf.constant([text], shape=(1, 1), dtype=tf.string, name='input_2')
+    predictions = model(text_tensor, training=False)
+    text_argmax = tf.argmax(predictions, axis =1)
+    class_names = ['Common Cold', 'Dengue', 'Malaria', 'Typhoid']
+    prediction = class_names[text_argmax[0]]
+
+    print(prediction)
+    
+    return f'{prediction}'
+
+
+@app.route('/')
+def index():
+    return 'Hey, Golie'
+
+if __name__ == '__main__':
+    app.run(debug=True)
